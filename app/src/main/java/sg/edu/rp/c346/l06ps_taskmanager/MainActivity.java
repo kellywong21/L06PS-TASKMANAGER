0ba3aec75de8668bf00f4e54ffd0e18d22fdf2e7
18004885 <18004885@myrp.edu.sg>
2020-05-29 15:42:17 +0800

    FINAL

--- app/src/main/java/sg/edu/rp/c346/l06ps_taskmanager/MainActivity.java
+++ app/src/main/java/sg/edu/rp/c346/l06ps_taskmanager/MainActivity.java
@@ -1,8 +1,11 @@
 package sg.edu.rp.c346.l06ps_taskmanager;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -13,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
 
     Button btnAddTasks;
     ListView lvTasks;
-    ArrayList<Task> al;
+    ArrayList<Task> taskList;
     ArrayAdapter aa;
 
 
@@ -21,5 +24,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        lvTasks = findViewById(R.id.lvTasks);
+        btnAddTasks = findViewById(R.id.btnAdd);
+        taskList = new ArrayList<>();
+
+        DBHelper db = new DBHelper(MainActivity.this);
+        taskList = db.getTasks();
+
+        aa = new TaskAdapter(MainActivity.this,R.layout.row,taskList);
+        lvTasks.setAdapter(aa);
+
+
+        btnAddTasks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this,AddActivity.class);
+                startActivityForResult(i,9);
+            }
+        });
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == 9){
+            DBHelper db = new DBHelper(MainActivity.this);
+            taskList.clear();
+            taskList.addAll(db.getTasks());
+            db.close();
+            lvTasks.setAdapter(aa);
+            aa.notifyDataSetChanged();
+        }
     }
 }
